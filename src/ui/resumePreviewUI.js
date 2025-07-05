e009a1e309f398b8ab90d9f6f09134fe33cd316c
Leandro Nunes <leandron7090@gmail.com>
2025-07-05 17:56:40 -0400

    feat: create link renderer and item renderer

--- src/ui/resumePreviewUI.js
+++ src/ui/resumePreviewUI.js
@@ -1,8 +1,26 @@
-import { currentLanguage, resumeData } from "../data/resumeData.js";
+import { currentLanguage } from "../data/resumeData.js";
 import { translations } from "../translations/i18n.js";
 
 export const updatePreview = async () => {
   const container = document.getElementById("resume-preview");
   const t = translations[currentLanguage];
   let html = "";
+
+  const renderLink = (url, label) => {
+    if (!url) return "";
+    const fullUrl = url.startsWith("http") ? url : `https://${url}`;
+
+    return `
+            <div>
+                <span>${label}: </span>
+                <a href="${fullUrl}" target="_blank" class="resume-link">${url}</a>
+            </div>
+        `;
+  };
+
+  const renderItem = (item, label) => {
+    if (!item) return "";
+
+    return `<div>${label}: ${item}</div>`;
+  };
 };
